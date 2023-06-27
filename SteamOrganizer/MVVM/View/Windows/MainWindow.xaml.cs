fc45d194a60ad2fd1f8b0a50491616b2bb82645b
Explyne <k.egazarov2004@gmail.com>
2023-06-28 00:13:53 +0300

    Added left menu expander Added menu

--- SteamOrganizer/MVVM/View/Windows/MainWindow.xaml.cs
+++ SteamOrganizer/MVVM/View/Windows/MainWindow.xaml.cs
@@ -2,15 +2,20 @@
 using SteamOrganizer.Infrastructure;
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace SteamOrganizer.MVVM.View.Windows
 {
     public sealed partial class MainWindow : Window
     {
-        private readonly CornerRadius TopPanelCornerRadius   = new CornerRadius(0d, 0d, 0d, 9d);
-        private readonly CornerRadius LeftBorderCornerRadius = new CornerRadius(9d, 9d, 0d, 0d);
+        private readonly CornerRadius TopPanelCornerRadius   = new CornerRadius(9d, 9d, 0d, 0d);
+        private readonly CornerRadius LeftBorderCornerRadius = new CornerRadius(0d, 0d, 0d, 9d);
         private readonly CornerRadius MainBorderCornerRadius = new CornerRadius(9d);
+
+        private bool IsMenuExpanderWaiting = false;
+
 
         public MainWindow()
         {
@@ -20,6 +25,8 @@ namespace SteamOrganizer.MVVM.View.Windows
         }
 
         #region View events
+
+        #region Global window events
         internal void OnWindowSourceInitialize(object sender, EventArgs e)
         {
             WindowMessageHandler.AddHook(this);
@@ -56,17 +63,73 @@ namespace SteamOrganizer.MVVM.View.Windows
 
             if (WindowState == WindowState.Maximized)
             {
-                WindowState = WindowState.Normal;
+                OnMaximizeWindow(null, null);
                 var position = Win32.GetMousePosition();
                 Left = position.X / 2;
                 Top = position.Y - 30;
-
             }
 
             DragMove();
         } 
         #endregion
 
+        #region Menu expander events
+        private void MenuExpanderOnGiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+            if (e.Effects == DragDropEffects.None)
+            {
+                e.UseDefaultCursors = false;
+                Mouse.SetCursor((sender as Border).Cursor);
+            }
+
+            var offsetY = Win32.GetMousePosition().X - (this.WindowState == WindowState.Maximized ? 0 : this.Left);
+
+            //TODO: Enum of panel positions and save to config
+            if (offsetY < 30 && LeftPanel.Width != 0)
+            {
+                LeftPanel.Width = 0;
+            }
+            else if (offsetY > 60 && offsetY < 100 && LeftPanel.Width != 70)
+            {
+                LeftPanel.Width = 70;
+            }
+            else if (offsetY > 180 && LeftPanel.Width != 200)
+            {
+                LeftPanel.Width = 200;
+            }
+
+            MenuExpanderOnMouseLeave(sender, null);
+            e.Handled = true;
+        }
+
+        private void MenuExpanderOnLeftMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DataObject data = new DataObject(DataFormats.Text, (Border)sender);
+
+            DragDrop.DoDragDrop((DependencyObject)e.Source, data, DragDropEffects.None);
+        }
+
+        private void MenuExpanderOnMouseEnter(object sender, MouseEventArgs e)
+            => (sender as Border).Child.Visibility = Visibility.Visible;
+
+        private async void MenuExpanderOnMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (IsMenuExpanderWaiting)
+                return;
+
+            IsMenuExpanderWaiting = true;
+
+            //This is needed if the method is called from MenuExpanderOnGiveFeedback()
+            (sender as Border).Child.Visibility = Visibility.Visible;
+
+            await System.Threading.Tasks.Task.Delay(3000);
+
+            (sender as Border).Child.Visibility = Visibility.Collapsed;
+            IsMenuExpanderWaiting = false;
+        }
+        #endregion
+
+        #endregion
 
         /// <summary>
         /// Sets rounded edges for the main window borders
