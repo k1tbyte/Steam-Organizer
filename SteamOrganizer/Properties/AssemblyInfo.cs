30b6ad31262eb156af585cacf6376baace8f5b51
k1tbyte <steelseries715@gmail.com>
2023-09-08 23:51:53 +0300

    2.0

--- SteamOrganizer/Properties/AssemblyInfo.cs
+++ SteamOrganizer/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Windows;
     ResourceDictionaryLocation.None, 
     ResourceDictionaryLocation.SourceAssembly 
 )]
-[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
