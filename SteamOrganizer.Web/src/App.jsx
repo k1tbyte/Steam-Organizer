69b65bd0f7afe22c8949dfc500d5c5401cff452c
kitbyte <steelseries715@gmail.com>
2024-03-28 18:57:37 +0200

    tailwind connected

--- SteamOrganizer.Web/src/App.jsx
+++ SteamOrganizer.Web/src/App.jsx
@@ -1,9 +1,12 @@
+import './assets/index.css'
+
 export default function App() {
     return (
         <div>
-            Test
+            <button className="bg-indigo-700 cursor-pointer m-10 font-bold p-3 rounded-xl hover:bg-indigo-600
+             hover:scale-110 transition-all text-gray-200">
+                Test button</button>
         </div>
-
     )
 }
 
